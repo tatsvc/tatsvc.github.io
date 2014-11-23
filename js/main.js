7db3a471ab10e4b067e9b5e96e34b440d612cb0c
tatsvc <tatiana.van.campenhout@gmail.com>
2014-11-22 20:57:09 -0500

    too much

--- js/main.js
+++ js/main.js
@@ -8,6 +8,20 @@
 		// animation end event name
 		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ];
 
+
+	var bodyEl = document.body,
+		content = document.querySelector( '.main-content' ),
+		openbtn = document.getElementById( 'open-button' ),
+		closebtn = document.getElementById( 'close-button' ),
+		isOpen = false,
+
+		morphEl = document.getElementById( 'morph-shape' ),
+		s = Snap( morphEl.querySelector( 'svg' ) );
+		path = s.select( 'path' );
+		initialPath = this.path.attr('d'),
+		pathOpen = morphEl.getAttribute( 'data-morph-open' ),
+		isAnimating = false;
+
 	function init() {
 		var onEndInitialAnimation = function() {
 			if( support.animations ) {
@@ -29,6 +43,8 @@
 		else {
 			onEndInitialAnimation();
 		}
+
+		initEvents();
 	}
 
 	function startLoading() {
@@ -72,6 +88,43 @@
 	function noscroll() {
 		window.scrollTo( 0, 0 );
 	}
+
+
+	function initEvents() {
+		openbtn.addEventListener( 'click', toggleMenu );
+		if( closebtn ) {
+			closebtn.addEventListener( 'click', toggleMenu );
+		}
+
+		// close the menu element if the target it´s not the menu element or one of its descendants..
+		content.addEventListener( 'click', function(ev) {
+			var target = ev.target;
+			if( isOpen && target !== openbtn ) {
+				toggleMenu();
+			}
+		} );
+	}
+
+	function toggleMenu() {
+		if( isAnimating ) return false;
+		isAnimating = true;
+		if( isOpen ) {
+			classie.remove( bodyEl, 'show-menu' );
+			// animate path
+			setTimeout( function() {
+				// reset path
+				path.attr( 'd', initialPath );
+				isAnimating = false; 
+			}, 300 );
+		}
+		else {
+			classie.add( bodyEl, 'show-menu' );
+			// animate path
+			path.animate( { 'path' : pathOpen }, 400, mina.easeinout, function() { isAnimating = false; } );
+		}
+		isOpen = !isOpen;
+	}
+
 
 	init();
 
