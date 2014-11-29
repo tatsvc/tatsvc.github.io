1b9793cec008cde2587921c9523548c03f4c2b17
tatsvc <tatiana.van.campenhout@gmail.com>
2014-11-29 16:10:49 -0500

    fuck IE

--- js/main.js
+++ js/main.js
@@ -25,6 +25,8 @@
 		isAnimating = false;
 
 	function init() {
+		detectIE();
+
 		if(window.location.hash) {
 			classie.add( container, 'alreadyloaded' );
 			classie.add( document.body, 'layout-switch' );
@@ -51,6 +53,28 @@
 		loadGrid();
 	}
 
+	function detectIE() {
+	    var ua = window.navigator.userAgent;
+	    var msie = ua.indexOf('MSIE ');
+	    var trident = ua.indexOf('Trident/');
+
+	    if (msie > 0) {
+	        // IE 10 or older => return version number
+	        	window.location.hash = 'loaded';
+	        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+
+	    }
+
+	    if (trident > 0) {
+	        // IE 11 (or newer) => return version number
+	        var rv = ua.indexOf('rv:');
+	        	window.location.hash = 'loaded';
+	        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+	    }
+
+	    // other browser
+	    return false;
+	}
 
 	function stickyNav(){    
 		console.log ('stickyNav');
